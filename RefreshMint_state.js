94f0d224d82ba3de25fcba96657aeb14f127e947
James Varvaro <supershospy@gmail.com>
2015-08-09 13:29:57 -0400

    Hot key support for refresh toggle. Refactor background process code

--- RefreshMint_state.js
+++ RefreshMint_state.js
@@ -9,35 +9,64 @@ window.onload = function() {
 			delete countArr[String(tabId)];
 		}
 	});
+	
 	chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
 		if(msg["type"] == "view") {
 			sendResponse(countArr[msg["tabId"]]);
 		} else if(msg["type"] == "begin") {
-			if(countArr.hasOwnProperty(String(msg["tabId"]))) {
-				window.clearInterval(countArr[String(msg["tabId"])].handle);
-				delete countArr[String(msg["tabId"])];
-			}
-			var handle = window.setInterval(function() {
-				if (countArr[String(msg["tabId"])].current > 0) {
-					var value = countArr[String(msg["tabId"])].current--;
-					chrome.browserAction.setBadgeText({text: String(value) + 's', tabId: msg["tabId"]});
-					chrome.runtime.sendMessage(null, {"type": "state", "tabId": msg["tabId"], "value": value});
-				} else {
-					countArr[String(msg["tabId"])].current = countArr[String(msg["tabId"])].interval;
-					chrome.tabs.reload(msg["tabId"], null, null);
-				}
-			}, 1000);
-			countArr[String(msg["tabId"])] = {
-				"interval": msg["value"],
-				"current": msg["value"],
-				"handle": handle
-			};
+			addRefresher(countArr, msg["tabId"], msg["value"]);
 		} else if(msg["type"] == "end") {
-			if(countArr.hasOwnProperty(String(msg["tabId"]))) {
-				chrome.browserAction.setBadgeText({text: "", tabId: msg["tabId"]});
-				window.clearInterval(countArr[String(msg["tabId"])].handle);
-				delete countArr[String(msg["tabId"])];
-			}
+			removeRefresher(countArr, msg["tabId"]);
+		}
+	});
+	
+	chrome.commands.onCommand.addListener(function(command) {
+		if(command == "toggleAutoRefresh") {
+			chrome.tabs.query({active: true}, function(result) {
+				if(result.length != 0) {
+					var tabId = result[0].id;
+					if(countArr.hasOwnProperty(String(tabId)) && countArr[String(tabId)].handle != null) {
+						// Already active, toggle off.
+						removeRefresher(countArr, tabId);
+					} else {
+						// Inactive, toggle on.
+						addRefresher(countArr, tabId, (countArr.hasOwnProperty(String(tabId))) ? countArr[String(tabId)].interval : 10);
+					}
+				}
+			});
 		}
 	});
 };
+
+function addRefresher(countArr, tabId, interval) {
+	var tabIdStr = String(tabId);
+	if(countArr.hasOwnProperty(tabIdStr)) {
+		window.clearInterval(countArr[tabIdStr].handle);
+		delete countArr[tabIdStr];
+	}
+	var handle = window.setInterval(function() {
+		if (countArr[tabIdStr].current > 0) {
+			var value = countArr[tabIdStr].current--;
+			chrome.browserAction.setBadgeText({text: String(value) + 's', tabId: tabId});
+			chrome.runtime.sendMessage(null, {"type": "state", "tabId": tabId, "value": value});
+		} else {
+			countArr[tabIdStr].current = countArr[tabIdStr].interval;
+			chrome.tabs.reload(tabId, null, null);
+		}
+	}, 1000);
+	countArr[tabIdStr] = {
+		"interval": interval,
+		"current": interval,
+		"handle": handle
+	};
+}
+
+function removeRefresher(countArr, tabId) {
+	var tabIdStr = String(tabId);
+	if(countArr.hasOwnProperty(tabIdStr)) {
+		chrome.browserAction.setBadgeText({text: "", tabId: tabId});
+		window.clearInterval(countArr[tabIdStr].handle);
+		countArr[tabIdStr].current = "";
+		countArr[tabIdStr].handle = null;
+	}
+}
